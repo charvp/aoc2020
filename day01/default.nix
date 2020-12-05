cf388abe8047d3728fd7a4f24eab4bd6e0f080cd
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-12-05 16:47:08 +0100

    Day 01

---
+++ day01/default.nix
@@ -0,0 +1,32 @@
+writeFile:
+with builtins;
+let
+  input = readFile ./input;
+  splitLines = filter isString (split "\n" input);
+  numbers = map fromJSON (filter (x: x != "") splitLines);
+  pairs = list:
+    if (length list > 0) then
+      let
+        y = head list;
+        t = tail list;
+      in
+      (map (x: { sum = x + y; product = x * y; }) t) ++ (pairs t)
+    else
+      [ ];
+  triples = list:
+    if (length list > 0) then
+      let
+        z = head list;
+        t = tail list;
+        ps = filter (x: x.sum < 2020) (pairs t);
+      in
+      (map ({ sum, product }: { sum = sum + z; product = product * z; }) ps) ++ (triples t)
+    else
+      [ ];
+  elem = head (filter (x: x.sum == 2020) (pairs numbers));
+  elem2 = head (filter (x: x.sum == 2020) (triples numbers));
+in
+{
+  day01-1 = writeFile "day01-1" (toString elem.product);
+  day01-2 = writeFile "day01-2" (toString elem2.product);
+}
---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "Advent of Code 2020";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils/master";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      { packages = builtins.foldl' (a: b: a // b) { } (map (day: import day pkgs.writeText) [ ./day01 ]); }
+    );
+}
