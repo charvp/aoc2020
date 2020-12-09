523c1eb42687b2f5a7a4a4101e791d4baf453182
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-12-09 11:45:54 +0100

    Optimise

--- day09/default.nix
+++ day09/default.nix
@@ -2,8 +2,13 @@ with builtins; with (import ../util.nix);
 let
   input = readFile ./input;
   numbers = map parseInt (splitNonEmptyLines input);
-  listForIndex = i: if i < 25 then [ ] else takeN 25 (dropN (i - 25) numbers);
-  correctSums = x: filter (p: p.sum == x.val) (pairs (listForIndex x.i));
+  windows = numbers: curRange:
+    let
+      correctSums = filter (p: (head numbers) == p.sum) (pairs curRange);
+      newNumbers = tail numbers;
+      newRange = (tail curRange) ++ [ (head numbers) ];
+    in
+    if length correctSums == 0 then head numbers else windows newNumbers newRange;
   contiguous = numbers: toFind: curRange: curSum:
     let
       added = curSum + (head numbers);
@@ -17,7 +22,10 @@ let
     if added == toFind then newRange else contiguous newNumbers toFind newRange newSum;
 in
 rec {
-  day09-1 = (head (filter (x: length (correctSums x) == 0) (dropN 25 (eachWithIndex numbers)))).val;
-  day09-2 = let range = contiguous numbers day09-1 [ ] 0; in
+  day09-1 = windows (dropN 25 numbers) (takeN 25 numbers);
+  day09-2 =
+    let
+      range = contiguous numbers day09-1 [ ] 0;
+    in
     (min range) + (max range);
 }
