f9d77458528c08c4425a774743514ebedd2fb66e
Charlotte Van Petegem <charlotte@vanpetegem.me>
2020-12-11 13:34:14 +0100

    Optimise

--- day11/default.nix
+++ day11/default.nix
@@ -2,23 +2,22 @@ with builtins; with (import ../util.nix);
 let
   input = readFile ./input;
   grid = map stringToList (splitNonEmptyLines input);
-  isFilled = grid: i: j:
-    let
-      row = elemAt grid i;
-      elem = elemAt row j;
-    in
-    i >= 0 && i < length grid && j >= 0 && j < length row && elem == "#";
-  countFilledNeighbours = grid: i: j:
-    length (filter (x: isFilled grid x.i x.j) [
-      { i = (i + 1); j = (j + 1); }
-      { i = (i + 1); j = (j - 1); }
-      { i = (i + 1); j = j; }
-      { i = (i - 1); j = (j + 1); }
-      { i = (i - 1); j = (j - 1); }
-      { i = (i - 1); j = j; }
-      { i = i; j = (j + 1); }
-      { i = i; j = (j - 1); }
-    ]);
+  isInBounds = { i, j }: i >= 0 && i < length grid && j >= 0 && j < length (elemAt grid i);
+  neighbourGrid =
+    genList
+      (i: genList
+        (j: filter isInBounds [
+          { i = (i + 1); j = (j + 1); }
+          { i = (i + 1); j = (j - 1); }
+          { i = (i + 1); j = j; }
+          { i = (i - 1); j = (j + 1); }
+          { i = (i - 1); j = (j - 1); }
+          { i = (i - 1); j = j; }
+          { i = i; j = (j + 1); }
+          { i = i; j = (j - 1); }
+        ])
+        (length (elemAt grid i)))
+      (length grid);
   visibleFor = grid: i: j: iDiff: jDiff:
     let
       row = elemAt grid i;
@@ -29,22 +28,29 @@ let
       newElem = elemAt newRow (j + jDiff);
     in
     if newI < 0 || newI >= length grid || newJ < 0 || newJ >= length row
-    then { i = newI; j = newJ; }
+    then null
     else if newElem == "."
     then visibleFor grid newI newJ iDiff jDiff
     else { i = newI; j = newJ; };
-  countFilledVisible = grid: i: j:
-    length (filter (x: isFilled grid x.i x.j) (map (x: visibleFor grid i j x.i x.j) [
-      { i = 1; j = 1; }
-      { i = 1; j = -1; }
-      { i = 1; j = 0; }
-      { i = -1; j = 1; }
-      { i = -1; j = -1; }
-      { i = -1; j = 0; }
-      { i = 0; j = 1; }
-      { i = 0; j = -1; }
-    ]));
-  golIter = fillCheck: numForStateChange: grid:
+  visibilityGrid =
+    genList
+      (i: genList
+        (j: filter isAttrs (map (x: visibleFor grid i j x.i x.j) [
+          { i = 1; j = 1; }
+          { i = 1; j = -1; }
+          { i = 1; j = 0; }
+          { i = -1; j = 1; }
+          { i = -1; j = -1; }
+          { i = -1; j = 0; }
+          { i = 0; j = 1; }
+          { i = 0; j = -1; }
+        ]))
+        (length (elemAt grid i)))
+      (length grid);
+  isFilled = grid: i: j: elemAt (elemAt grid i) j == "#";
+  countFilled = checkGrid: grid: i: j:
+    length (filter (x: isFilled grid x.i x.j) (elemAt (elemAt checkGrid i) j));
+  golIter = checkGrid: numForStateChange: grid:
     genList
       (i:
         let
@@ -54,7 +60,7 @@ let
           (j:
             let
               elem = elemAt row j;
-              filledOthers = fillCheck grid i j;
+              filledOthers = countFilled checkGrid grid i j;
             in
             if elem == "L" && filledOthers == 0
             then "#"
@@ -65,8 +71,8 @@ let
           (length row)
       )
       (length grid);
-  endStateP1 = fix (golIter countFilledNeighbours 4) grid;
-  endStateP2 = fix (golIter countFilledVisible 5) grid;
+  endStateP1 = fix (golIter neighbourGrid 4) grid;
+  endStateP2 = fix (golIter visibilityGrid 5) grid;
   occupied = state: sum (map (x: length (filter (y: y == "#") x)) state);
 in
 {
